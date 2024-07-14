8aa9860fee638c2fefa176197e17729dd4cafb40
BhaveshMihani <bhaveshmihani07@gmail.com+BhaveshMihani@users.noreply.github.com>
2024-07-14 12:17:04 +0530

    plain login page

---
+++ WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1 style="margin-left: 480px">Login Page</h1>
+        <div style="margin-left: 440px">
+            <asp:Label ID="Label1" runat="server" Text="UserName"></asp:Label>
+            :<asp:TextBox ID="TextBox1" runat="server" Width="166px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            Password:<asp:TextBox ID="TextBox2" runat="server" Width="166px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myconn"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string uname = TextBox1.Text;
+                string pwd = TextBox2.Text;
+                con.Open();
+                string qry = "select * from Login_tbl where UserName = '" + uname + "' and PassWord ='"+pwd+"'";
+                SqlCommand cmd = new SqlCommand(qry, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read()) { Label2.Text = "Login Sucessful"; }
+                else { Label2.Text = "Incorrect Credential"; }
+            }
+
+            catch(Exception ex)
+            {
+                Label2.Text=ex.Message;
+            }
+        }
+    }
+}
